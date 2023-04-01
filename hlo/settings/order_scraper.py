1f2202c91db85245ec8141d527f28a224917af92
Anders Einar Hilden <hildenae@gmail.com>
2023-04-01 23:53:04 +0200

    New settings, new lauch

--- hlo/settings/order_scraper.py
+++ hlo/settings/order_scraper.py
@@ -2,18 +2,24 @@ import os
 import os.path
 from pathlib import Path
 
-from django.core.exceptions import ImproperlyConfigured
-
 from .django import env
 
+# Config for scraper command
 SCRAPER_CACHE_BASE: str = Path(env('SCRAPER_CACHE_BASE', default='./scraper-cache')).resolve()
+SCRAPER_PDF_PRINTER: str = env(
+    'SCRAPER_PDF_PRINTER',
+    default="Microsoft Print to PDF" if os.name == "nt" else "Mozilla Save to PDF")
 
-_DEFAULT_PRINTER = "Microsoft Print to PDF" if os.name == "nt" else "Mozilla Save to PDF"
-SCRAPER_PDF_PRINTER: str = env('SCRAPER_PDF_PRINTER', default=_DEFAULT_PRINTER)
-
+# Config for scraper aliexpress command
 SCRAPER_ALI_USERNAME: str = env('SCRAPER_ALI_USERNAME', default=None)
-SCRAPER_ALI_PASSWORD: str = env('SCRAPER_ALI_PASSWORD', default=None)
 SCRAPER_ALI_PASSWORD: str = env('SCRAPER_ALI_PASSWORD', default=None)
 SCRAPER_ALI_ORDERS = env.list('SCRAPER_ALI_ORDERS', default=[])
 SCRAPER_ALI_ORDERS_SKIP = env.list('SCRAPER_ALI_ORDERS_SKIP', default=[])
 SCRAPER_ALI_ORDERS_MAX: int = env('SCRAPER_ALI_ORDERS_MAX', default=-1)
+
+# Config for scraper amazon.de command
+SCRAPER_AMZDE_USERNAME: str = env('SCRAPER_AMZDE_USERNAME', default=None)
+SCRAPER_AMZDE_PASSWORD: str = env('SCRAPER_AMZDE_PASSWORD', default=None)
+SCRAPER_AMZDE_ORDERS_MAX: int = env('SCRAPER_AMZDE_ORDERS_MAX', default=-1)
+SCRAPER_AMZDE_ORDERS = env.list('SCRAPER_AMZDE_ORDERS_SKIP', default=[])
+SCRAPER_AMZDE_ORDERS_SKIP = env.list('SCRAPER_AMZDE_ORDERS_SKIP', default=[])
--- order_scraper/management/commands/scrapers/amazon_de.py
+++ order_scraper/management/commands/scrapers/amazon_de.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+# This is used in a Django command
 from django.core.management.base import BaseCommand, CommandError
+# from django.core.serializers.json import DjangoJSONEncoder
 
 from .base import BaseScraper
 
@@ -6,7 +9,7 @@ class AmazonDeScraper(BaseScraper):
     def __init__(self, command: BaseCommand, cache_orderlist: bool):
         super().__init__(command)
         self.cache_orderlist = cache_orderlist
-        raise CommandError('Scraping of Amazon (DE) not yet implemented')
 
     def command_scrape(self):
-        pass
+        # https://www.amazon.de/-/en/gp/css/order-history?ref_=nav_orders_first
+        print(settings.SCRAPER_AMZDE_PASSWORD)
