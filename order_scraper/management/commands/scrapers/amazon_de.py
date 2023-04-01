5757644bbd837d0804c5942d2afe15ffb9b4f742
Anders Einar Hilden <hildenae@gmail.com>
2023-04-01 23:36:36 +0200

    Do not in any way start on amazon.de scraper

--- order_scraper/management/commands/scrapers/amazon_de.py
+++ order_scraper/management/commands/scrapers/amazon_de.py
@@ -3,6 +3,10 @@ from django.core.management.base import BaseCommand, CommandError
 from .base import BaseScraper
 
 class AmazonDeScraper(BaseScraper):
-    def __init__(self, command: BaseCommand):
+    def __init__(self, command: BaseCommand, cache_orderlist: bool):
         super().__init__(command)
+        self.cache_orderlist = cache_orderlist
         raise CommandError('Scraping of Amazon (DE) not yet implemented')
+
+    def command_scrape(self):
+        pass
