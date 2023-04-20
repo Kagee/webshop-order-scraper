c3eaf20fd8afaefbb72e8e54c70ad72809c200d1
Anders Einar Hilden <hildenae@gmail.com>
2023-04-20 20:41:21 +0200

    pimoroni-item-page-vask

---
+++ order_scraper/management/commands/scrapers/pimoroni.py
@@ -0,0 +1,30 @@
+# remove:
+# div#menu_brand_account
+# div#benefits
+# div#navigation => to ganger? dukker opp på nytt n¨år vindu rezices? mange elementer med samme id *facepalm*
+# div#currency_picker
+# footer#footer
+# div#right
+# div#search
+# section#reviews
+# section#user_photos
+# div#lightbox
+# div#WebPixelsManagerSandboxContainer
+
+
+# css-endringer
+
+# tag body unset font-family overflow-y --webkit-font-smoothin
+# tag html unset --theme-font --body-font
+# tag main displaygrid -> block
+# div#navigation mobile-only -> display none (dukker opp når resize) se over, duplisert id
+# url-fiks fra adafruit
+# section#gallery unset display (grid)
+# section#gallery set float => right + margin-left 2em
+# section#gallery -> img -> data-lightbox -> /cdn/shop/products/2633-01_1500x1500.jpg?v=1436978298
+#    slett alle bortsett fra siste A/IMG
+#    set max-sice på siste img til 250px
+# h1 unset font-size, litt vel stor
+# div#description p:first-of-type -> unset font-size
+
+# --use_cached_orderlist
