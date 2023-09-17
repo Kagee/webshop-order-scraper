e3770d9377f6250e3f339bbb5bf269f158de8bc5
Anders Einar Hilden <hildenae@gmail.com>
2023-09-17 20:03:44 +0200

    Remove unsed "tryout" scraper

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -8,6 +8,5 @@ from .imap import IMAPScraper
 from .pimoroni import PimoroniScraper
 from .ebay import EbayScraper
 from .kjell import KjellScraper
-from .tryout import TryOutScraper
 from .polyalkemi import PolyalkemiScraper
 from .utils import RED, BLUE, AMBER, GREEN
